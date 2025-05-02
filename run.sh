3f50f5ad86aaf5a3277a1243c1b8f321b4eba574
Izzy Farley <izzy@fondutech.com>
2025-05-02 10:29:25 -0700

    build and run

---
+++ run.sh
@@ -0,0 +1,5 @@
+echo "===== Activating virtual environment ====="
+source venv/bin/activate
+
+echo "===== Starting the mcp Server Application ====="
+exec python3 mcp_fondu_search_user_context/run.py
